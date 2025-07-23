e98e2ba81c20e05b0d89c002c6e85ac0b70c5bf0
Pratik Ardhapurkar <praardha@amazon.com>
2025-07-23 20:32:55 +0530

    fe file commits

--- PR_3_java/java/src/detectors/insecure_cryptography/InsecureCryptographyNonCompliant.java
+++ PR_3_java/java/src/detectors/insecure_cryptography/InsecureCryptographyNonCompliant.java
@@ -8,8 +8,10 @@ import java.security.NoSuchAlgorithmException;
 public class InsecureCryptographyNonCompliant {
 
     // Noncompliant: Key size is insufficient for cryptographic algorithm.
+    // Note: NIST recommends at least 2048 bits for RSA keys for adequate security.
     public void nonCompliant() throws NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        // This is deliberately using an insecure key size for demonstration
         keyPairGenerator.initialize(128);
         keyPairGenerator.genKeyPair();
     }
